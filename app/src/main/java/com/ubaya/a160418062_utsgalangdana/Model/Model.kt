d918aba1524ccdca892b6e638330f78730102a57
s160418062 <phizzarfernos@gmail.com>
2021-09-27 22:00:46 +0700

    First Fragment (Main)

---
+++ app/src/main/java/com/ubaya/a160418062_utsgalangdana/Model/Model.kt
@@ -0,0 +1,14 @@
+package com.ubaya.a160418062_utsgalangdana.Model
+
+data class GalangDana(
+    val id:String?,
+    val nama:String?,
+    val keterangan:String?,
+    val pemilik:String?,
+    val Url:String
+)
+
+data class User(
+    val name:String?,
+    val password:String?
+)
