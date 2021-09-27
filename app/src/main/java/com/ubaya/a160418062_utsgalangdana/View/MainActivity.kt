ebddf601da780a23dda4768d1b67348067c70a63
s160418062 <phizzarfernos@gmail.com>
2021-09-27 12:28:51 +0700

    MainActivity

---
+++ app/src/main/java/com/ubaya/a160418062_utsgalangdana/View/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.ubaya.a160418062_utsgalangdana.View
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ubaya.a160418062_utsgalangdana.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
